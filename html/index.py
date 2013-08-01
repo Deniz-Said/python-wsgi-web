2675061683f40352bfa887af7ac5eb9f3d5332f6
Deniz Said <deniz@dsitmanagement.com>
2013-08-01 22:04:53 +0200

    Testing change propagation

--- html/index.py
+++ html/index.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = 'Hello World! asda sdj askdjaskdj '
+    output = 'Hello World! asda sdj askdjaskdj zzzzzzzzzzzzzzzzz '
 
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
